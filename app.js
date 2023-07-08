9119916693247655023d4afd88c1820505581a4a
Azevedo <jazevedo0406@gmail.com>
2023-07-08 17:46:53 +0100

    add css

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-let n = 4;
+let n = 6;
 
 const container = document.querySelector('#container');
 const board = document.querySelector('#board');
+const range = document.querySelector('#side');
+const p = document.querySelector('p');
 
-board.style.cssText += `grid-template-columns: repeat(auto-fit,${400/n}px);`;
-
-function createBoard(n) {
-    board.innerHTML = '';
-    for(let i = 0; i<(n*n); i++) {
-        const div = document.createElement('div');
-        div.classList.add('block');
-        board.appendChild(div);
-    }
-}
-
-createBoard(n)
-
-const squares = document.querySelectorAll('.block');
+let squares = document.querySelectorAll('.block');
 const colorPicker = document.querySelector('#color');
 
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => square.style.backgroundColor = colorPicker.value);
-});
+function createBoard(grid) {
+    board.innerHTML = '';
+    for(let i = 0; i<(grid*grid); i++) {
+        const div = document.createElement('div');
+        div.classList.add('block');
+        board.setAttribute("draggable", false);
+        board.appendChild(div);
+    }
+    board.style.gridTemplateColumns = `repeat(auto-fit,${400/grid}px)`;
+    updateSquares();
+    p.innerHTML = (`${grid} x ${grid}`)
+}
+
+//On mouse over it changes the color of the block
+function updateSquares() {
+    squares = document.querySelectorAll('.block');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => square.style.backgroundColor = colorPicker.value);
+    });
+}
+
+createBoard(range.value);
+
+range.addEventListener('input', () => createBoard(range.value));
+
 
